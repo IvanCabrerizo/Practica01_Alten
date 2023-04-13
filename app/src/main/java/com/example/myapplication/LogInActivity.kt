5cb48ed3476a179339d35d60b0a7d3f3554c3e7b
icabrerizo <ivan.cabrerizo@alten.es>
2023-04-13 12:55:10 +0200

    Implemented TextWatcher, and advanced in the validation of email and password

--- app/src/main/java/com/example/myapplication/LogInActivity.kt
+++ app/src/main/java/com/example/myapplication/LogInActivity.kt
@@ -2,9 +2,12 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.text.method.PasswordTransformationMethod
 import com.example.myapplication.databinding.ActivityLogInBinding
 
-class LogInActivity : AppCompatActivity() {
+class LogInActivity : AppCompatActivity(), TextWatcher {
 
     private lateinit var binding: ActivityLogInBinding
 
@@ -12,5 +15,45 @@ class LogInActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLogInBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.loginInputEmail.addTextChangedListener(this)
+
+        binding.loginInputPassword.addTextChangedListener(this)
+
+        binding.loginBtnViewPassword.setOnClickListener {
+            if(binding.loginInputPassword.transformationMethod == null){
+                binding.loginInputPassword.transformationMethod = PasswordTransformationMethod()
+            }
+            else{
+                binding.loginInputPassword.transformationMethod = null
+            }
+        }
+    }
+
+    fun checkMail(mail: String): Boolean{
+        val regex = Regex("^([\\w\\.\\-]+)@([\\w\\-]+)((\\.(\\w){2,3})+)\$")
+        return regex.matches(mail)
+    }
+
+    fun checkPassword(password: String): Boolean{
+        val regex = Regex("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{6,8}$")
+        return regex.matches(password)
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        if (checkPassword(binding.loginInputPassword.text.toString()) && checkMail(binding.loginInputEmail.text.toString())){
+            binding.loginBtnEnter.isEnabled = true
+        }
+        else{
+            binding.loginBtnEnter.isEnabled = false
+        }
+    }
+
+    override fun afterTextChanged(s: Editable?) {
+
     }
 }
