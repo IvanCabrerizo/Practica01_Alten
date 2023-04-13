27e529cfa6ff185806abd11eaec03f6756cb632f
icabrerizo <ivan.cabrerizo@alten.es>
2023-04-13 08:38:32 +0200

    Data binding implemented and base logIn UI completed

--- app/src/main/java/com/example/myapplication/LogInActivity.kt
+++ app/src/main/java/com/example/myapplication/LogInActivity.kt
@@ -2,10 +2,15 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.databinding.ActivityLogInBinding
 
 class LogInActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLogInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_log_in)
+        binding = ActivityLogInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
