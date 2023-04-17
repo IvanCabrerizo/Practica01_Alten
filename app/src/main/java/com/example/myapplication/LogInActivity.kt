98b77b945590243ce773a2decfb489125685a4ab
icabrerizo <ivan.cabrerizo@alten.es>
2023-04-17 10:23:45 +0200

    Implemented the 1 second pause and the user validation with a data class and three users. I have also created an extension function for the toasts

--- app/src/main/java/com/example/myapplication/LogInActivity.kt
+++ app/src/main/java/com/example/myapplication/LogInActivity.kt
@@ -1,11 +1,15 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.security.keystore.UserPresenceUnavailableException
 import android.util.Log
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
 import com.example.myapplication.databinding.ActivityLogInBinding
+import java.util.logging.Handler
 
 class LogInActivity : AppCompatActivity() {
 
@@ -16,12 +20,33 @@ class LogInActivity : AppCompatActivity() {
         binding = ActivityLogInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val usersList = listOf(
+            User("manolo@gmail.com", "Manolo92"),
+            User("paco@gmail.com", "Paco92"),
+            User("maria@gmail.com", "Maria92")
+        )
+
         binding.loginInputEmail.addTextChangedListener {
             binding.loginBtnEnter.isEnabled = checkMail(binding.loginInputEmail.text.toString()) && checkPassword(binding.loginInputPassword.text.toString())
         }
 
         binding.loginInputPassword.addTextChangedListener {
             binding.loginBtnEnter.isEnabled = checkMail(binding.loginInputEmail.text.toString()) && checkPassword(binding.loginInputPassword.text.toString())
+        }
+
+        binding.loginBtnEnter.setOnClickListener {
+            Thread {
+                runOnUiThread {
+                    binding.loginBtnEnter.isEnabled = false
+                    binding.loginBtnEnter.text = "Cargando"
+                }
+                Thread.sleep(1000)
+                runOnUiThread{
+                    binding.loginBtnEnter.isEnabled = true
+                    binding.loginBtnEnter.text = "Entrar"
+                    checkLoginData(binding.loginInputEmail.text.toString(), binding.loginInputPassword.text.toString(), usersList)
+                }
+            }.start()
         }
     }
 
@@ -33,5 +58,18 @@ class LogInActivity : AppCompatActivity() {
     fun checkPassword(password: String): Boolean {
         val regex = Regex("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{6,8}$")
         return regex.matches(password)
+    }
+
+    fun checkLoginData(mail: String, password: String, userList: List<User>){
+        val userFound = userList.find { user -> user.email == mail && user.password == password}
+        val mailFound = userList.find { user -> user.email == mail }
+        val passwordFound = userList.find { user -> user.password == password }
+
+        when {
+            mailFound?.email != mail -> showToast("No existe el email")
+            passwordFound?.password != password -> showToast("No existe la contraseña")
+            userFound == null -> showToast("No existen usuario ni contraseña")
+            else -> showToast("Datos correctos")
+        }
     }
 }
